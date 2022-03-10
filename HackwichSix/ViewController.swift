a93d3a79f1c3a1bd94a582e364264d3008faca0e
Cliff Bailey <69611676+phazonxiii@users.noreply.github.com>
2022-03-10 13:29:14 -1000

    Completed all parts - worked on first try - hurray!!

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var myFriendsArray = ["Jerry", "Peterson", "Hildegard"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return myFriendsArray.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        let text = myFriendsArray[indexPath.row]
+        cell?.textLabel?.text = text
+        return cell!
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
