193166f8e4818f79d5225a684c6da32b96bf24f3
Cliff Bailey <69611676+phazonxiii@users.noreply.github.com>
2022-03-25 17:18:48 -1000

    It's just not working after troubleshooting, after following video and guidelines multiple times, with granular detail - just not working.

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -11,28 +11,14 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var myFriendsArray = ["Jerry", "Peterson", "Hildegard"]
+    
+   // @IBOutlet var tableView: UITableView!
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var countriesToVisit = ["The Netherlands", "South Africa", "Cambodia", "Turkey", "Canada"]
+    
     var friendsHomeArray = ["Charlotte", "Baltimore", "Weatherford"]
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        return myFriendsArray.count
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
-        let text = myFriendsArray[indexPath.row]
-        cell?.textLabel?.text = text
-        cell?.detailTextLabel?.text = friendsHomeArray[indexPath.row]
-        return cell!
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        
-        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
-        
-    }
     
     var restarurantImageData = [String]()
     
@@ -46,7 +32,35 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         restarurantImageData = dict!.object(forKey:"restaurantImages") as! [String]
         
     }
-
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+           tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return myFriendsArray.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        let text = myFriendsArray[indexPath.row]
+        cell?.detailTextLabel?.text = friendsHomeArray[indexPath.row]
+        cell?.textLabel?.text = text
+        
+        return cell!
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier == "mySegue"
+        {
+            let s1 = segue.destination as! detailViewController
+            let imageIndex = tableView.indexPathfForSelectedRow?.row
+            s1.imagePass = restarurantImageData[imageIndex!]
+        }
+    }
 }
 
---
+++ detailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  detailViewController.swift
+//  HackwichSix
+//
+//  Created by Clifford Bailey on 2203/25/.
+//  Copyright © 2022 Cliff Bailey. All rights reserved.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var imagePass: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let imageName = imagePass {imageView.image = UIImage(named: imageName)}
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
